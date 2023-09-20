2142788136d7b92a85da69ee5e67cbda4e4dc887
unknown <lisandro.batiancila@gmail.com>
2023-09-21 05:40:38 +0800

    [feature][san] vehicle file uploading

--- src/controllers/my-property/my-property.controller.ts
+++ src/controllers/my-property/my-property.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { AnyFilesInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { MyPropertyService } from 'src/service/my-property/my-property.service';
 
 @Controller('my-property')
-export class MyPropertyController {}
+export class MyPropertyController {
+    constructor(private propertyService: MyPropertyService) {
+
+    }
+
+    @Post("vehicle")
+    @UseInterceptors(FilesInterceptor('images'))
+    uploadVehicleProperty(@UploadedFiles() images: Array<Express.Multer.File>) {
+        // console.log(formBody);
+        console.log(images);
+    }
+}
