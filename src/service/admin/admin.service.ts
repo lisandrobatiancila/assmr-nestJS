201e2c48e9f2724770022c54beeacc83bb356e9a
unknown <lisandro.batiancila@gmail.com>
2023-11-07 06:30:48 +0800

    [san-feat-26] admin side

--- src/service/admin/admin.service.ts
+++ src/service/admin/admin.service.ts
@@ -27,82 +27,151 @@ export class AdminService {
     @InjectRepository(Realeststate)
     private realestateEntity: Repository<Realeststate>,
     @InjectRepository(Jewelry)
-    private Jewelry: Repository<Jewelry>,
+    private jewelryEntity: Repository<Jewelry>,
     @InjectRepository(HouseAndLot) private halEntity: Repository<HouseAndLot>,
     @InjectRepository(House) private houseEntity: Repository<House>,
     @InjectRepository(Lot) private lotEntity: Repository<Lot>,
     @InjectRepository(Property) private propertyEntity: Repository<Property>,
   ) {}
-  async getHistory(): Promise<ResponseData<any>> {
-    let concatResult = [];
-    const vehicle = await this.vehicleEntity
-      .createQueryBuilder('vehicle')
-      .innerJoin(Assumer, 'assumer')
-      .innerJoin(Assumption, 'assumpt')
-      .innerJoin(User, 'userAssumer')
-      .innerJoin(User, 'userOwner')
-      .innerJoin(Property, 'property')
-      .where('vehicle.propertyId = assumpt.propertyId')
-      .andWhere('userAssumer.id = assumpt.userId')
-      .andWhere('userOwner.id = property.userId')
-      .andWhere('property.id = assumpt.propertyId')
-      .select([
-        'vehicle',
-        'userOwner',
-        'userAssumer',
-        'assumer',
-        'assumpt',
-        'property',
-      ])
-      .groupBy('assumpt.id')
-      .getRawMany();
+  async getHistory(param: {
+    historyValue: string;
+  }): Promise<ResponseData<any>> {
+    let concatResult: any = [];
+    const { historyValue } = param;
+    switch (historyValue) {
+      case 'accept/succesffull transaction':
+        break;
+      case 'cancelled transaction':
+        break;
+      case 'assumed transaction':
+        break;
+      case 'posted property':
+        const Pvehicle = await this.vehicleEntity
+          .createQueryBuilder('vehicle')
+          .innerJoin(Property, 'property')
+          .where('vehicle.isDropped = 1 AND property.id = vehicle.propertyId')
+          .getRawMany();
+        const Prealestate = await this.realestateEntity
+          .createQueryBuilder('realestate')
+          .innerJoin(Property, 'property')
+          .where(
+            'realestate.isDropped = 1 AND property.id = realestate.propertyId',
+          )
+          .getRawMany();
+        const Pjewelry = await this.jewelryEntity
+          .createQueryBuilder('jewelry')
+          .innerJoin(Property, 'property')
+          .where('jewelry.isDropped = 1 AND property.id = jewelry.propertyId')
+          .getRawMany();
+        concatResult = {
+          type: 'posted property',
+          result: [...Pvehicle, ...Prealestate, ...Pjewelry],
+        };
+        break;
+      case 'deleted property':
+        const Dvehicle = await this.vehicleEntity
+          .createQueryBuilder('vehicle')
+          .innerJoin(Property, 'property')
+          .where('vehicle.isDropped = 1 AND property.id = vehicle.propertyId')
+          .select(['vehicle', 'property'])
+          .getRawMany();
+        const Drealestate = await this.realestateEntity
+          .createQueryBuilder('realestate')
+          .innerJoin(Property, 'property')
+          .where(
+            'realestate.isDropped = 1 AND property.id = realestate.propertyId',
+          )
+          .select(['realestate', 'property'])
+          .getRawMany();
+        const Djewelry = await this.jewelryEntity
+          .createQueryBuilder('jewelry')
+          .innerJoin(Property, 'property')
+          .where('jewelry.isDropped = 1 AND property.id = jewelry.propertyId')
+          .select(['jewelry', 'property'])
+          .getRawMany();
+        concatResult = {
+          type: 'deleted property',
+          result: [...Dvehicle, ...Drealestate, ...Djewelry],
+        };
+        break;
+      case 'on-going transaction':
+        const ONvehicle = await this.vehicleEntity
+          .createQueryBuilder('vehicle')
+          .innerJoin(Assumer, 'assumer')
+          .innerJoin(Assumption, 'assumpt')
+          .innerJoin(User, 'userAssumer')
+          .innerJoin(User, 'userOwner')
+          .innerJoin(Property, 'property')
+          .where('vehicle.propertyId = assumpt.propertyId')
+          .andWhere('userAssumer.id = assumpt.userId')
+          .andWhere('userOwner.id = property.userId')
+          .andWhere('property.id = assumpt.propertyId')
+          .select([
+            'vehicle',
+            'userOwner',
+            'userAssumer',
+            'assumer',
+            'assumpt',
+            'property',
+          ])
+          .groupBy('assumpt.id')
+          .getRawMany();
 
-    const realestate = await this.realestateEntity
-      .createQueryBuilder('realestate')
-      .innerJoin(Assumer, 'assumer')
-      .innerJoin(Assumption, 'assumpt')
-      .innerJoin(User, 'userAssumer')
-      .innerJoin(User, 'userOwner')
-      .innerJoin(Property, 'property')
-      .where('realestate.propertyId = assumpt.propertyId')
-      .andWhere('realestate.propertyId = property.id')
-      .andWhere('userAssumer.id = assumpt.userId')
-      .andWhere('userOwner.id = property.userId')
-      .andWhere('property.id = assumpt.propertyId')
-      .select([
-        'realestate',
-        'userOwner',
-        'userAssumer',
-        'assumer',
-        'assumpt',
-        'property',
-      ])
-      .groupBy('assumpt.id')
-      .getRawMany();
+        const ONrealestate = await this.realestateEntity
+          .createQueryBuilder('realestate')
+          .innerJoin(Assumer, 'assumer')
+          .innerJoin(Assumption, 'assumpt')
+          .innerJoin(User, 'userAssumer')
+          .innerJoin(User, 'userOwner')
+          .innerJoin(Property, 'property')
+          .where('realestate.propertyId = assumpt.propertyId')
+          .andWhere('realestate.propertyId = property.id')
+          .andWhere('userAssumer.id = assumpt.userId')
+          .andWhere('userOwner.id = property.userId')
+          .andWhere('property.id = assumpt.propertyId')
+          .select([
+            'realestate',
+            'userOwner',
+            'userAssumer',
+            'assumer',
+            'assumpt',
+            'property',
+          ])
+          .groupBy('assumpt.id')
+          .getRawMany();
 
-    const jewelry = await this.Jewelry.createQueryBuilder('jewelry')
-      .innerJoin(Assumer, 'assumer')
-      .innerJoin(Assumption, 'assumpt')
-      .innerJoin(User, 'userAssumer')
-      .innerJoin(User, 'userOwner')
-      .innerJoin(Property, 'property')
-      .where('jewelry.propertyId = assumpt.propertyId')
-      .andWhere('jewelry.propertyId = property.id')
-      .andWhere('userAssumer.id = assumpt.userId')
-      .andWhere('userOwner.id = property.userId')
-      .andWhere('property.id = assumpt.propertyId')
-      .select([
-        'jewelry',
-        'userOwner',
-        'userAssumer',
-        'assumer',
-        'assumpt',
-        'property',
-      ])
-      .groupBy('assumpt.id')
-      .getRawMany();
+        const ONjewelry = await this.jewelryEntity
+          .createQueryBuilder('jewelry')
+          .innerJoin(Assumer, 'assumer')
+          .innerJoin(Assumption, 'assumpt')
+          .innerJoin(User, 'userAssumer')
+          .innerJoin(User, 'userOwner')
+          .innerJoin(Property, 'property')
+          .where('jewelry.propertyId = assumpt.propertyId')
+          .andWhere('jewelry.propertyId = property.id')
+          .andWhere('userAssumer.id = assumpt.userId')
+          .andWhere('userOwner.id = property.userId')
+          .andWhere('property.id = assumpt.propertyId')
+          .select([
+            'jewelry',
+            'userOwner',
+            'userAssumer',
+            'assumer',
+            'assumpt',
+            'property',
+          ])
+          .groupBy('assumpt.id')
+          .getRawMany();
 
-    concatResult = [...vehicle, ...realestate, ...jewelry];
+        concatResult = {
+          type: 'on-going transaction',
+          result: [...ONvehicle, ...ONrealestate, ...ONjewelry],
+        };
+        break;
+      default:
+        console.log('No historyValue');
+    }
+    // console.log(concatResult);
 
     return {
       code: 200,
